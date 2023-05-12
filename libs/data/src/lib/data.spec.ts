858df12af0402f93b3fd93342cd216fc241f0802
Nik Cherednik <ighosta9@gmail.com>
2023-05-12 15:20:44 +0400

    chore: test changed

--- libs/data/src/lib/data.spec.ts
+++ libs/data/src/lib/data.spec.ts
@@ -3,6 +3,6 @@ import { calculateROI } from './data';
 describe('Calculate ROI', () => {
   it('should work', () => {
     const result = calculateROI({ revenue: 100, cost: 50 });
-    expect(result).toEqual(1);
+    expect(result).toEqual(2);
   });
 });

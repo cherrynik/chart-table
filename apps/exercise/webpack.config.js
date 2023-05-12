fbd803b33985c5357b1fb18d1699da7142eb0882
Nikolay Cherednik <32453685+cherrynik@users.noreply.github.com>
2023-05-12 14:05:38 +0400

    fix: finally fixed blank page

--- apps/exercise/webpack.config.js
+++ apps/exercise/webpack.config.js
@@ -4,10 +4,9 @@ const { withReact } = require('@nx/react');
 // Nx plugins for webpack.
 module.exports = composePlugins(withNx({
   skipTypeChecking: true,
-}), withReact(), (config) => {
-  // Fix github pages blank page issue.
-  config.output.publicPath = process.env.NODE_ENV === 'production' ? '/chart-table/' : '/';
-
+}), withReact({
+  baseHref: process.env.NODE_ENV === 'production' ? '/chart-table/' : '/',
+}), (config) => {
   // Update the webpack config as needed here.
   // e.g. `config.plugins.push(new MyPlugin())`
   return config;

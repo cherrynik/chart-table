9f4889a47fa64629748a9830a13f084c522a2d6c
Nikolay Cherednik <32453685+cherrynik@users.noreply.github.com>
2023-05-12 13:51:43 +0400

    chore: trying to fix blank gh-page

--- apps/exercise/webpack.config.js
+++ apps/exercise/webpack.config.js
@@ -5,6 +5,9 @@ const { withReact } = require('@nx/react');
 module.exports = composePlugins(withNx({
   skipTypeChecking: true,
 }), withReact(), (config) => {
+  // Fix github pages blank page issue.
+  config.output.publicPath = process.env.NODE_ENV === 'production' ? '/chart-table/' : '/';
+
   // Update the webpack config as needed here.
   // e.g. `config.plugins.push(new MyPlugin())`
   return config;

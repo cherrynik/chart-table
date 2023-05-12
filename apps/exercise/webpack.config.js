32840d2a306dda7c61cd468b0306bd73333fb268
Nikolay Cherednik <32453685+cherrynik@users.noreply.github.com>
2023-05-12 14:10:52 +0400

    feat: workflow

--- apps/exercise/webpack.config.js
+++ apps/exercise/webpack.config.js
@@ -4,7 +4,10 @@ const { withReact } = require('@nx/react');
 // Nx plugins for webpack.
 module.exports = composePlugins(withNx({
   skipTypeChecking: true,
-}), withReact(), (config) => {
+}), withReact({
+  // TODO: Move to env
+  baseHref: process.env.NODE_ENV === 'production' ? '/chart-table/' : '/',
+}), (config) => {
   // Update the webpack config as needed here.
   // e.g. `config.plugins.push(new MyPlugin())`
   return config;
